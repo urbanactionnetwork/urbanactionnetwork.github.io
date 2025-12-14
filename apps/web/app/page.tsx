1689fea9ef73681ecbc356499b05d61cc1df1cc8
funa <photiary@gmail.com>
2025-12-15 08:45:15 +0900

    요구 프롬프트 추가, 컴포넌트 및 이미지 추가

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,6 @@
-import Link from 'next/link'
-import { Button } from '@workspace/ui/components/button'
-
 export default function Page() {
   return (
-    <div className="flex min-h-svh items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">
-          <Link href="/template/info">Button</Link>
-        </Button>
-      </div>
+    <div>
     </div>
   )
 }
